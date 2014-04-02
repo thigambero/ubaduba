3f39017379424325e87536dca27b9a511a395fa1
Vitor Novo <vitornovos@gmail.com>
2014-04-02 15:18:24 -0300

    Alterações login e turmas

--- funcoes/login.php
+++ funcoes/login.php
@@ -2,18 +2,38 @@
 function login($usuario, $senha){
 	$senha = md5($senha);
 	$db = conectaBD();
-	$query = "SELECT * FROM admins WHERE usuario = '".$usuario."' AND senha = '".$senha."'";
+	$query = "SELECT permissao FROM logins WHERE usuario = '".$usuario."' AND senha = '".$senha."'";
 	$result = mysql_query($query);
 	if(mysql_num_rows($result) > 0){
 		$row = mysql_fetch_array($result);
+		$permissao = $row['permissao'];
 
-		$query = "INSERT INTO log_acesso (pk_acesso, ip, navegador, fk_admin, status)
-		VALUES (NULL,'".$_SERVER['REMOTE_ADDR']."', '".$_SERVER['HTTP_USER_AGENT']."', '".$row['pk_admin']."',  1);";
+		switch ($permissao) {
+			case '1':
+				$query = "SELECT nome FROM alunos WHERE ra = '$usuario'";
+				break;
+			case '2':
+				$query = "SELECT nome FROM professores WHERE rp = '$usuario'";
+				break;
+		}
+
+		if($permissao != 10){
+			$result = mysql_query($query);
+			$row2 = mysql_fetch_array($result);
+			$nome = $row2['nome'];
+		}
+		else{
+			$nome = "Administrador";
+		}
+			
+
+		$query = "INSERT INTO log_acesso (pk_acesso, ip, navegador, usuario, permissao, status)
+		VALUES (NULL,'".$_SERVER['REMOTE_ADDR']."', '".$_SERVER['HTTP_USER_AGENT']."', '$usuario', '$permissao', 1);";
 		$result = mysql_query($query);
 
-		$_SESSION['admin'] = $row['pk_admin'];
-		$_SESSION['permissao'] = $row['permissao'];
-		$_SESSION['nome'] = $row['nome'];
+		$_SESSION['usuario'] = $usuario;
+		$_SESSION['permissao'] = $permissao;
+		$_SESSION['nome'] = $nome;
 		return true;
 	}
 	else{
@@ -22,7 +42,7 @@ function login($usuario, $senha){
 }
 
 function logout(){
-	unset($_SESSION['pk_admin']);
+	unset($_SESSION['usuario']);
 	unset($_SESSION['permissao']);
 	unset($_SESSION['nome']);
 	echo "<meta HTTP-EQUIV='Refresh' CONTENT='0;URL=./'>";
