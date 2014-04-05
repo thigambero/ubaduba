e6442c33c5146318d3c232cc2c3ba243266aee8b
Vitor Novo <vitornovos@gmail.com>
2014-04-05 18:55:17 -0300

    Banco de dados local

--- funcoes/db.php
+++ funcoes/db.php
@@ -1,10 +1,15 @@
 <?php
 function conectaBD() {
 
-    $db_host='72.249.76.79'; //O nome do servidor
-    $db_user='control_projeto'; //O nome do utilizador de MySQL
-    $db_password='ubaduba'; //A senha do utilizador
-    $db_name='control_projeto'; //O nome da base de dados
+    // $db_host='72.249.76.79'; //O nome do servidor
+    // $db_user='control_projeto'; //O nome do utilizador de MySQL
+    // $db_password='ubaduba'; //A senha do utilizador
+    // $db_name='control_projeto'; //O nome da base de dados
+
+    $db_host='192.168.1.132'; //O nome do servidor
+    $db_user='root'; //O nome do utilizador de MySQL
+    $db_password='root'; //A senha do utilizador
+    $db_name='sistema'; //O nome da base de dados
 
         
     $db=mysql_connect($db_host, $db_user, $db_password);
